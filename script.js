f7354e209f45a5b75f363189d71acd048ffe8da7
Johnny Simpson <johnnysimpson@Johnnys-MacBook-Pro.local>
2020-12-04 13:42:25 +0000

    clip-path changes

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 document.addEventListener('DOMContentLoaded', function(e) {
+    // Lets just call out the elements we want to focus on
     const mainEl = '.shredded-paper';
     const repeatUnit = '.shredded-paper .content';
-    const repeatNum = 10;
+    const repeatNum = 10; // And this is how many times we want the element to be repeated
     
     for(let i = 0; i < repeatNum; ++i) {
+        // We will create a new element for every repeatNum
         let newEl = document.createElement('div');
         newEl.innerHTML = document.querySelector(repeatUnit).innerHTML;
         newEl.classList.add(`item-${i+1}`);
-        newEl.setAttribute('style', `clip: rect(0,${(i+1)*40}px,300px,${i*40}px);`);
+        // Our element is about 400px wide, so I have hardcoded this in here
+        // But it is not too challenging to customise if you want
+        // THIS IS WHERE WE SET CLIP()
+        newEl.setAttribute('style', `clip-path: inset(0px ${(repeatNum-1-i)*40}px 0 ${i*40}px);`);
+        // And we append the element
         document.querySelector(mainEl).appendChild(newEl);
     }
 
+    // Finally, we add the 'animate' class when a user clicks on shred.
     document.querySelector('.shred-me').addEventListener('click', function(e) {
         document.querySelector('.shredded-paper').classList.add('animate');
     });
